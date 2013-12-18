20821a2c1d5b6f1df2bfab3f7c4e75b8a371feb2
Christoph Wille <christoph.wille@gmail.com>
2013-12-18 10:09:58 +0100

    Monitor Qando in addition to OGD API (compare which one is available at which times)

--- Source/local/app.js
+++ Source/local/app.js
@@ -4,6 +4,35 @@ function serviceAvailabilityCallback(logItem) {
     console.log(logItem);
 }
 
-console.log(monitoring.formatTestUrl());
+// console.log(monitoring.formatTestUrl());
 // monitoring.checkServiceAvailability(serviceAvailabilityCallback);
 
+
+var req = require('request');
+
+function checkQandoAvailability(callback) {
+    req( {uri: 'http://isqandoupdev.azurewebsites.net/api/qando' }, function (err, response, body) {
+        if (!err && response.statusCode == 200) {
+            try {
+                var json = JSON.parse(body);
+
+                if (json.Succeeded && json.Lines) {
+                    if (json.Succeeded === true && json.Lines.length !== 0) {
+                        callback(true);
+                        return;
+                    }
+                }
+            }
+            catch (jsonParseEx) {
+            }
+        }
+
+        callback(false);
+    });
+}
+
+function qandoCallback(isUp) {
+    console.log(isUp);
+}
+
+checkQandoAvailability(qandoCallback);
--- Source/service/scheduler/monitorRealtimeApi.js
+++ Source/service/scheduler/monitorRealtimeApi.js
@@ -4,14 +4,48 @@ function monitorRealtimeApi() {
     monitoring.checkServiceAvailability(serviceAvailabilityCallback);
 
     function serviceAvailabilityCallback(logItem) {
-         var logTable = tables.getTable('AvailabilityLog');
-    
-         logTable.insert(logItem, {
-             success: function () {
-                // do something here (if necessary)
-             }
-         });
-    
-        console.log(logItem);
+        // writeLogEntry(logItem);   // Use this if not monitoring Qando too
+        
+        checkQandoAvailability(qandoCallback);
+        
+        function qandoCallback(isUp) {
+            logItem.isQandoUp = isUp;
+            writeLogEntry(logItem);
+        }
     }
+}
+
+function writeLogEntry(logItem) {
+     var logTable = tables.getTable('AvailabilityLog');
+
+     logTable.insert(logItem, {
+         success: function () {
+            // do something here (if necessary)
+         }
+     });
+
+    // console.log(logItem);
+}
+
+var req = require('request');
+
+function checkQandoAvailability(callback) {
+    req( {uri: 'http://isqandoupdev.azurewebsites.net/api/qando' }, function (err, response, body) {
+        if (!err && response.statusCode == 200) {
+            try {
+                var json = JSON.parse(body);
+
+                if (json.Succeeded && json.Lines) {
+                    if (json.Succeeded === true && json.Lines.length !== 0) {
+                        callback(true);
+                        return;
+                    }
+                }
+            }
+            catch (jsonParseEx) {
+            }
+        }
+
+        callback(false);
+    });
 }
